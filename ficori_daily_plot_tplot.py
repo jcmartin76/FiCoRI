f63b6e7f7ad4539e3a5feb6b2157c691fefb55e0
jcmartin76 <juanaus@gmail.com>
2018-09-03 02:39:25 -0700

    Add files via upload

--- ficori_daily_plot_tplot.py
+++ ficori_daily_plot_tplot.py
@@ -1,6 +1,3 @@
-#import matplotlib
-#matplotlib.use('Agg')
-
 import numpy
 import numpy as np
 import datetime
@@ -9,22 +6,22 @@ import pytplot
 import calendar
 
 import os
-import imgkit                            
+import imgkit
 
-#from shutil import copyfile
+from shutil import copyfile
 
-#import tweepy
+import tweepy
 
 import socket
 
 ##############
 # This function is the twitter API setup.
-#def setup_api():
-#  auth = tweepy.OAuthHandler('6qIx6jJ5WxB9fN3H7dg7yE0T4', 
-#        'tlb7R74SP0kMiLJuJbzpzSB8ir85wPgWdDpUa8Yr90olJB4S08')
-#  auth.set_access_token('967497555675246593-BiWkgrNs6HaIOU0468cLui1BQt7mSHA',
-#        'N4gBMN5Ezw3KvtwFM21cxisKlAhdAXmy3pkEOfzTB3T9V')
-#  return tweepy.API(auth)
+def setup_api():
+  auth = tweepy.OAuthHandler('6qIx6jJ5WxB9fN3H7dg7yE0T4', 
+        'tlb7R74SP0kMiLJuJbzpzSB8ir85wPgWdDpUa8Yr90olJB4S08')
+  auth.set_access_token('967497555675246593-BiWkgrNs6HaIOU0468cLui1BQt7mSHA',
+        'N4gBMN5Ezw3KvtwFM21cxisKlAhdAXmy3pkEOfzTB3T9V')
+  return tweepy.API(auth)
 
 def chunkIt(seq, num):
     avg = len(seq) / float(num)
@@ -52,87 +49,154 @@ def easyAverage(datetimeList): #----> Func Declaration
     return averageTimeInTimeStampFormat
     #timeInHumanReadableForm
 
+
 ###### Processing program
+def processing_data(idate, directory, Syndirory):
 
-idate = datetime.datetime.today() - datetime.timedelta(2)
+    '''if os.path.exists('/Users/oliveros/Sync/ficori_test/images/ficori_plot_%d%02d%02d.jpg'%(idate.year,idate.month,idate.day)):'''
+    if os.path.exists(Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.html'%
+(idate.year,idate.month,idate.day)):
+        print('Spectra already plotted %s'%datetime.datetime.today())
 
-props = dict(boxstyle='round', facecolor='orange', alpha=0.5)
+    else:
+        try:        
+            api=setup_api()
+            print('Preparing Spectra')
+            filea = directory+'Data_Auto_Ant_A_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
+            fileb = directory+'Data_Auto_Ant_B_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
+            filec = directory+'Data_Cross_phase_Ant_AB_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
+            print("%s\n%s\n%s\n"%(filea,fileb,filec))
 
-directory='./'
+            #   Read and load data and time 
+            timestamp = numpy.loadtxt(filea, delimiter=' ',dtype='str', usecols=(0,1))
 
-print('Preparing Spectra')
-filea = directory+'Data_Auto_Ant_A_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
-fileb = directory+'Data_Auto_Ant_B_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
-filec = directory+'Data_Cross_phase_Ant_AB_%d%02d%02d.dat.gz'%(idate.year,idate.month,idate.day)
-print("%s\n%s\n%s\n"%(filea,fileb,filec))
+            #   Timestamp array definition and fix
+            timestamp = numpy.apply_along_axis(lambda d: d[0] + ' ' + d[1], 1, timestamp)
+            dates_list=[]
+            for date in timestamp:
+                try:
+                    stDate = date
+                    dates_list.append(datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f'))
+            #            print(stDate,' try')
 
-#   Read and load data and time 
-timestamp = numpy.loadtxt(filea, delimiter=' ',dtype='str', usecols=(0,1))
+                except:
+                    stDate = datetime.datetime.strptime(date,'%Y-%m-%d %H:%M:%S') + datetime.timedelta(microseconds=1)
+                    dates_list.append(stDate)
 
-#   Timestamp array definition and fix
-timestamp = numpy.apply_along_axis(lambda d: d[0] + ' ' + d[1], 1, timestamp)
-dates_list=[]
-for date in timestamp:
-    try:
-        stDate = date
-        dates_list.append(datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f'))
-#            print(stDate,' try')
-
-    except:
-        stDate = datetime.datetime.strptime(date,'%Y-%m-%d %H:%M:%S') + datetime.timedelta(microseconds=1)
-        dates_list.append(stDate)
-
-dates_list = np.array([easyAverage(data1a) for data1a in chunkIt(dates_list,1439)])
-tt = [calendar.timegm(i.timetuple()) for i in dates_list]             
-
-
-dataa = numpy.loadtxt(filea, delimiter=' ',dtype='float',usecols=range(2,1026))
-dataa = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(dataa,1439)])
-
-datab = numpy.loadtxt(fileb, delimiter=' ',dtype='float',usecols=range(2,1026))
-datab = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(datab,1439)])
-
-dataab = np.abs(numpy.loadtxt(filec, delimiter=' ',dtype='complex',usecols=range(2,1026)))
-dataab = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(dataab,1439)])
+            dates_list = np.array([easyAverage(data1a) for data1a in chunkIt(dates_list,1439)])
+            tt = [calendar.timegm(i.timetuple()) for i in dates_list]             
 
 
-print('Data loaded...')
+            dataa = numpy.loadtxt(filea, delimiter=' ',dtype='float',usecols=range(2,1026))
+            dataa = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(dataa,1439)])
 
-#   Frequency array definition
-Fr=numpy.linspace(780000,800780000,1024)/1000000.
+            datab = numpy.loadtxt(fileb, delimiter=' ',dtype='float',usecols=range(2,1026))
+            datab = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(datab,1439)])
 
-pytplot.store_data('Auto West', data={'x':tt,'y':dataa,'v':Fr})
-pytplot.store_data('Auto East', data={'x':tt,'y':datab,'v':Fr})
-pytplot.store_data('Cross W-E', data={'x':tt,'y':dataab,'v':Fr})
+            dataab = np.abs(numpy.loadtxt(filec, delimiter=' ',dtype='complex',usecols=range(2,1026)))
+            dataab = np.array([np.mean(data1a,axis=0) for data1a in chunkIt(dataab,1439)])
 
-del dataa,datab,dataab
+            print('Data loaded...')
 
-#Options Antenna West
-pytplot.options('Auto West', 'spec', 1)
-pytplot.options('Auto West', 'Colormap', 'rainbow')
-pytplot.options('Auto West', 'ylog', 0)
-pytplot.options('Auto West', 'ytitle', 'Frequency (MHz)')
-pytplot.options('Auto West', 'ztitle', 'Power (a.u.)')
-#pytplot.options('Auto West', 'Colorbar', 0)
+            #   Frequency array definition
+            Fr=numpy.linspace(780000,800780000,1024)/1000000.
 
-#Options Antenna West
-pytplot.options('Auto East', 'spec', 1)
-pytplot.options('Auto East', 'Colormap', 'rainbow')
-pytplot.options('Auto East', 'ylog', 0)
-pytplot.options('Auto East', 'ytitle', 'Frequency (MHz)')
-pytplot.options('Auto East', 'ztitle', 'Power (a.u.)')
-#pytplot.options('Auto East', 'Colorbar', 0)
+            pytplot.store_data('Auto West', data={'x':tt,'y':dataa,'v':Fr})
+            pytplot.store_data('Auto East', data={'x':tt,'y':datab,'v':Fr})
+            pytplot.store_data('Cross W-E', data={'x':tt,'y':dataab,'v':Fr})
 
-#Options Cross E-W
-pytplot.options('Cross W-E', 'spec', 1)
-pytplot.options('Cross W-E', 'Colormap', 'rainbow')
-pytplot.options('Cross W-E', 'ylog', 0)
-pytplot.options('Cross W-E', 'ytitle', 'Frequency (MHz)')
-pytplot.options('Cross W-E', 'ztitle', 'Power (a.u.)')
-#pytplot.options('Cross W-E', 'Colorbar', 0)
+            del dataa,datab,dataab
 
-pytplot.tplot([0,1,2],bokeh=True,save_file='test.html')
+            #Options Antenna West
+            pytplot.options('Auto West', 'spec', 1)
+            pytplot.options('Auto West', 'Colormap', 'rainbow')
+            pytplot.options('Auto West', 'ylog', 0)
+            pytplot.options('Auto West', 'ytitle', 'Frequency (MHz)')
+            pytplot.options('Auto West', 'ztitle', 'Power (a.u.)')
+            #pytplot.options('Auto West', 'Colorbar', 0)
 
-#Converting html output to jpg
-#imgkit.from_file('test.html', 'test.jpg')
+            #Options Antenna West
+            pytplot.options('Auto East', 'spec', 1)
+            pytplot.options('Auto East', 'Colormap', 'rainbow')
+            pytplot.options('Auto East', 'ylog', 0)
+            pytplot.options('Auto East', 'ytitle', 'Frequency (MHz)')
+            pytplot.options('Auto East', 'ztitle', 'Power (a.u.)')
+            #pytplot.options('Auto East', 'Colorbar', 0)
+
+            #Options Cross E-W
+            pytplot.options('Cross W-E', 'spec', 1)
+            pytplot.options('Cross W-E', 'Colormap', 'rainbow')
+            pytplot.options('Cross W-E', 'ylog', 0)
+            pytplot.options('Cross W-E', 'ytitle', 'Frequency (MHz)')
+            pytplot.options('Cross W-E', 'ztitle', 'Power (a.u.)')
+            #pytplot.options('Cross W-E', 'Colorbar', 0)
+
+            pytplot.tplot([0,1,2],bokeh=True,save_file=Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.html'%(dates_list[0].year,dates_list[0].month,dates_list[0].day))
+
+            #Converting html output to jpg
+            imgkit.from_file(
+            Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.html'%(dates_list[0].year,dates_list[0].month,dates_list[0].day),
+            Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.jpg'%(dates_list[0].year,dates_list[0].month,dates_list[0].day))
+
+            #Last Spectra copy
+
+            copyfile(Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.html'%(dates_list[0].year,dates_list[0].month,dates_list[0].day), 
+            Syndirory+'ficori_test/images/latest_spectra.html')
+
+            copyfile(Syndirory+'ficori_test/images/ficori_plot_%d%02d%02d.jpg'%(dates_list[0].year,dates_list[0].month,dates_list[0].day), 
+            Syndirory+'ficori_test/images/latest_spectra.jpg')
+
+            
+            # Send the tweet.
+            fn = Syndirory+'ficori_test/images/latest_spectra.jpg'
+            status = '%d-%02d-%02d radio spectrum, testing new imaging algorithm. Posted by the FiCoRI bot.'%(dates_list[0].year,dates_list[0].month,dates_list[0].day)
+
+            api.update_with_media(fn, status=status)
+            print('tweet posted')
+            
+        except:
+            print('There was an error, data maybe corrupt or not yet on server')
+
+def main():                      # Define the main function
+    ###### dates range definition
+
+    # testing value
+    date = [datetime.datetime.today() - datetime.timedelta(5)]
+
+    # For nomal ops 
+    #date = datetime.datetime.today() - datetime.timedelta(1)
+
+    # For reprocessing
+
+    #date_array = [datetime.datetime.today() - datetime.timedelta(x) for  x in range (15)]
+    #date_array = date_array[1::]
+    #date_array.sort()
+
+    ###### Directories
+    namehost = socket.gethostname()
+
+    if namehost == 'raspberrypi':
+        directory = '/media/pi/ficori_hdd/'
+        Syndirory = '/home/pi/Sync/'
+
+    if namehost == 'sue.ssl.berkeley.edu':
+        directory = '/Users/oliveros/Box Sync/ficori/'
+        Syndirory = '/Volumes/Mnemosyne/Sync/'
+
+    if namehost == 'ra':
+        directory = './'
+        Syndirory = './'
+
+    print(namehost, directory, Syndirory)
+
+    ##### Calling processing function
+    print(len(date))
+    if len(date) == 1:
+        processing_data(date[0], directory, Syndirory)
+    else:
+        for date in date_array:
+            processing_data(date, directory, Syndirory)
+
+#Running main
+main()
 
